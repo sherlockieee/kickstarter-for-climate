89199a49b45e6145726a7980c8a0dad990ed051e
sherlockieee <phuonghatrannguyen00@gmail.com>
2022-08-08 14:11:10 -0700

    update lint error

--- nextjs-exploration/pages/about.jsx
+++ nextjs-exploration/pages/about.jsx
@@ -22,18 +22,9 @@ export default function About() {
       </p>
 
       <p>
-        The old saying "a picture is worth a thousand words" is quite cliche.
-        But that doesn't make it any less true, especially in marketing and
-        particularly in the instant-gratification, short attention span world we
-        live in today. Getting folks to retain (or even register) your messages
-        and content or take action is harder than ever, especially if all you
-        are giving them is words.
-      </p>
-
-      <p>
         Images are stronger than words. However, the fast-moving nature of GIFs
         make them stronger than images and their shorter length make them more
-        digestible than video. That's the short answer.
+        digestible than video. That is the short answer.
       </p>
     </div>
   );
