fdb80fa1ad52d43abae51c19c180096a0539bbbc
sherlockieee <phuonghatrannguyen00@gmail.com>
2022-08-12 14:00:14 -0700

    update alemic

--- fastapi-demo/alembic/env.py
+++ fastapi-demo/alembic/env.py
@@ -3,6 +3,9 @@ from logging.config import fileConfig
 from sqlalchemy import engine_from_config, create_engine
 from sqlalchemy import pool
 import os
+from dotenv import load_dotenv
+load_dotenv()
+
 from alembic import context
 
 # this is the Alembic Config object, which provides
