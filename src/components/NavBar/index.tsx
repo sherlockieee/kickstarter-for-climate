5842e7c0b119b27f74cae40b0909c69cb45d74a6
sherlockieee <phuonghatrannguyen00@gmail.com>
2022-09-10 12:35:30 -0700

    add apply link and add browse project link

--- src/components/NavBar/index.tsx
+++ src/components/NavBar/index.tsx
@@ -3,41 +3,46 @@ import theme from "../../constants/theme";
 import { NextLinkComposed } from "../Link";
 
 export const NavBar = () => {
-  return (
-    <Box sx={{ display: "flex" }}>
-      <AppBar
-        component="nav"
-        color="transparent"
-        position="sticky"
-        style={{ boxShadow: "none", padding: "1rem" }}
-      >
-        <Toolbar>
-          <Typography
-            variant="h5"
-            style={{ flexGrow: 1, color: theme.palette.secondary.main }}
-          >
-            X | Kickstarter for Climate
-          </Typography>
-          <Box style={{ display: "flex", gap: `1rem` }}>
-            <Button
-              variant="outlined"
-              color="primary"
-              component={NextLinkComposed}
-              to={{ pathname: "/apply" }}
-            >
-              Apply for funding{" "}
-            </Button>
-            <Button
-              variant="contained"
-              color="primary"
-              component={NextLinkComposed}
-              to={{ pathname: "/projects" }}
-            >
-              Browse projects
-            </Button>
-          </Box>
-        </Toolbar>
-      </AppBar>
-    </Box>
-  );
+	return (
+		<Box sx={{ display: "flex" }}>
+			<AppBar
+				component="nav"
+				color="transparent"
+				position="sticky"
+				style={{ boxShadow: "none", padding: "1rem" }}
+			>
+				<Toolbar>
+					<Typography
+						variant="h5"
+						style={{
+							flexGrow: 1,
+							color: theme.palette.secondary.main,
+						}}
+					>
+						X | Kickstarter for Climate
+					</Typography>
+					<Box style={{ display: "flex", gap: `1rem` }}>
+						<Button
+							variant="outlined"
+							color="primary"
+							component={NextLinkComposed}
+							to="https://forms.gle/v5MZhi4pvoCLMyfa7"
+							target="_blank"
+							rel="noopener noreferrer"
+						>
+							Apply for funding{" "}
+						</Button>
+						<Button
+							variant="contained"
+							color="primary"
+							component={NextLinkComposed}
+							to={{ pathname: "/projects" }}
+						>
+							Browse projects
+						</Button>
+					</Box>
+				</Toolbar>
+			</AppBar>
+		</Box>
+	);
 };
--- src/views/landingPage/buttons.tsx
+++ src/views/landingPage/buttons.tsx
@@ -2,28 +2,30 @@ import { Button } from "@material-ui/core";
 import { NextLinkComposed } from "../../components/Link";
 
 type ButtonOverrideProps = {
-  variant?: "contained" | "text" | "outlined";
-  color?: "primary" | "secondary";
+	variant?: "contained" | "text" | "outlined";
+	color?: "primary" | "secondary";
 };
 
 export const ProjectButton = (props: ButtonOverrideProps) => (
-  <Button
-    variant={props.variant ? props.variant : "contained"}
-    color={props.color ? props.color : "primary"}
-    component={NextLinkComposed}
-    to={{ pathname: "/projects" }}
-  >
-    Browse projects
-  </Button>
+	<Button
+		variant={props.variant ? props.variant : "contained"}
+		color={props.color ? props.color : "primary"}
+		component={NextLinkComposed}
+		to={{ pathname: "/projects" }}
+	>
+		Browse projects
+	</Button>
 );
 
 export const ApplyButton = (props: ButtonOverrideProps) => (
-  <Button
-    variant={props.variant ? props.variant : "contained"}
-    color={props.color ? props.color : "primary"}
-    component={NextLinkComposed}
-    to={{ pathname: "/apply" }}
-  >
-    Apply for funding
-  </Button>
+	<Button
+		variant={props.variant ? props.variant : "contained"}
+		color={props.color ? props.color : "primary"}
+		component={NextLinkComposed}
+		to="https://forms.gle/v5MZhi4pvoCLMyfa7"
+		target="_blank"
+		rel="noopener noreferrer"
+	>
+		Apply for funding
+	</Button>
 );
